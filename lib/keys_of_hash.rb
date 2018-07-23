23d958d7e79bb1a90b66be1abe5897a864ef1b04
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-23 20:49:36 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@ require "pry"
 class Hash
   def keys_of(arguments)
     :animals.each do |key, value|
-      return *key if value == arguments
+      p *key if value == arguments
     end
   end
 end
